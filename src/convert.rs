369633c9564752773512cf1e15e4eaee29921b71
suhanyujie <suhanyujie@qq.com>
2023-06-12 10:53:58 +0800

    add one test fn

--- src/convert.rs
+++ src/convert.rs
@@ -1,10 +1,20 @@
 use calamine::{open_workbook, Reader, Xlsx};
 
 fn convert_to_json_file() {
-    let mut excel: Xlsx<_> = open_workbook("file.xlsx").unwrap();
+    let mut excel: Xlsx<_> = open_workbook("./example/data/table1.xlsx").unwrap();
     if let Some(Ok(r)) = excel.worksheet_range("Sheet1") {
         for row in r.rows() {
             println!("row={:?}, row[0]={:?}", row, row[0]);
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_convert_to_json_file1() {
+        convert_to_json_file();
     }
 }
